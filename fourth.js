ef8d47e619e62325c40c412095b59ae585b8dcdc
Valmiki Vishwakarma <you@example.com>
2022-12-28 17:31:51 +0530

    add.

---
+++ fourth.js
@@ -0,0 +1,14 @@
+var age =10000
+console.log(age)
+console.log("Age of the best sportmen in the world: ",age)
+console.log("Age of the best sportmen in the world: "+age)
+
+
+/*
+echo "# Javascript28Dec2022" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/automatioexpert/Javascript28Dec2022.git
+git push -u origin main */
